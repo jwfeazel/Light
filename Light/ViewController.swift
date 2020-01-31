13777c76cdd06bbe2f2326697201e9c509e53459
Joey Feazel <jwfeazel@gmail.com>
2020-01-30 16:07:11 -0800

    removed button name, simplified color change code

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -10,20 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = true
+   @IBOutlet weak var lightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateUI()
     }
-    
-    var lightOn = true
-
     @IBAction func buttonPressed(_ sender: UIButton) {
         lightOn = !lightOn
-        if lightOn {
-            view.backgroundColor = .white
-        } else {
-            view.backgroundColor = .black
-        }
+        updateUI()
+        
+   }
+    
+    
+    func updateUI() {
+        
+        //if lightOn {
+        //    view.backgroundColor = .white
+            //lightButton.setTitle("Off", for: .normal)
+       // } else {
+       //     view.backgroundColor = .black
+           // lightButton.setTitle("On", for: .normal)
+        //}
+        view.backgroundColor = lightOn ? .white : .black
     }
     
 }
